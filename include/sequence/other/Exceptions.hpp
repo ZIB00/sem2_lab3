081ebe13119267c8b8d32149d68fb6ad461bac7d
ZIB00 <alexeypobedaabedoqyexela@gmail.com>
2026-05-26 02:51:09 +0300

    PriorityQueue v1.2

---
+++ include/sequence/other/Exceptions.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <stdexcept>
+#include <string>
+
+class InvalidArgument : public std::invalid_argument
+{
+    public:
+        using std::invalid_argument::invalid_argument;
+};
+
+class OutOfRange : public std::out_of_range
+{
+    public:
+        using std::out_of_range::out_of_range;
+};
+
+class LogicError : public std::logic_error
+{
+    public:
+        using std::logic_error::logic_error;
+};
+
+class OtherError : public std::exception
+{
+    public:
+        using std::exception::exception;
+};
+
+class BadOptionalAccess : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "bad optional access: optional is empty";
+    }
+};
